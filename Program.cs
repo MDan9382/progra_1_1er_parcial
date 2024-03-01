c3d56a9571aa42dce74d3ce641e45b3b00475b6f
MDan9382 <160198160+MDan9382@users.noreply.github.com>
2024-02-29 22:06:53 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int n=0, rn;
+Random r = new Random();
+rn = r.Next(1, 101);
+
+Console.WriteLine("adivina un numero");
+do
+{
+    
+    n = Convert.ToInt32(Console.ReadLine());
+    if (rn < n)
+    {
+        Console.WriteLine("el numero secreto es menor, intente otra vez");
+    }
+    else if (rn > n)
+    {
+        Console.WriteLine("el numero secreto es mayor, intente otra vez");
+    }
+    else
+    {
+        Console.WriteLine("¡correcto! \nel numero secreto es " + rn);
+    }
+} while (rn != n);
